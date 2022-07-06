b1bf2effd41a9aeebec25e8389a92b169b976beb
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-07-06 10:26:39 -0700

    Fetching of data from Json file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
-import cardData from "./cardData";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Card from "./components/Card";
+// import cardData from "./cardData";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import memesData from "./memesData";
 import "./App.css";
 
+// function App() {
+//   const singleCard = cardData.map((card) => {
+//     return <Card key={card.id} card={card} />;
+//   });
+//   return (
+//     <div>
+//       <Navbar />
+//       <Hero />
+//       <section className="card--list">{singleCard}</section>
+//     </div>
+//   );
+// }
+
 function App() {
-  const singleCard = cardData.map((card) => {
-    return <Card key={card.id} card={card} />;
+  const totalMemes = memesData.data.memes;
+  let singleMeme;
+  console.log(totalMemes);
+  singleMeme = totalMemes.map((meme) => {
+    return console.log(meme);
   });
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <section className="card--list">{singleCard}</section>
+      <Header />
+      <Meme />
     </div>
   );
 }
