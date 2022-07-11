608d649f53c573fc5f45bb68d832fb6190a29d86
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-07-11 03:00:56 -0700

    Update to text in meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,14 +3,21 @@ import memesData from "../memesData";
 
 export default function Meme() {
   const [meme, setMeme] = useState({
-    topText: "You see me",
-    bottomText: "We see us",
+    topText: "",
+    bottomText: "",
     randomImg: "https://i.imgflip.com/1bhf.jpg",
   });
 
   const [allMemeImages, setAllMemeImages] = useState(memesData);
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
 
-  function getMemeImage() {
+  function getMemeImage(event) {
     const memesArr = allMemeImages.data.memes;
     const randNum = Math.floor(Math.random() * memesArr.length);
     const url = memesArr[randNum].url;
@@ -23,13 +30,31 @@ export default function Meme() {
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button onClick={getMemeImage} className="form--button">
           Get a meme image
         </button>
       </div>
-      <img className="meme--image" src={meme.randomImg} alt="random meme" />
+      <div className="meme">
+        <img className="meme--image" src={meme.randomImg} alt="random meme" />
+        <h2 className="meme--text_top">{meme.topText}</h2>
+        <h2 className="meme--text_bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
